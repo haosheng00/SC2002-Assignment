91e9bc8183501f1d612dd4d63496740826e0c9ef
3Clovers <3clovers0806@gmail.com>
2022-10-25 14:43:31 +0800

    init movie class

---
+++ src/Classes/Enum.java
@@ -0,0 +1,53 @@
+package Classes;
+
+public class Enum {
+    
+    /* Movie */
+
+    // MovieStatus
+    public enum MovieStatus {
+        COMING_SOON("Coming soon"),
+        PREVIEW("Preview"),
+        END_OF_SHOWING("End of showing"),
+        NOW_SHOWING("Now showing");
+
+        private String movieStatus;
+        MovieStatus(String movieStatus) {
+            this.movieStatus = movieStatus;
+        }
+    }
+
+    // MovieGenre
+    public enum MovieGenre {
+        ACTION("Action"),
+        HORROR("Horror"),
+        THRILLER("Thriller"),
+        ADVENTURE("Adventure"),
+        COMEDY("Comedy"),
+        ANIME("Anime"),
+        ROMANCE("Romance"),
+        DRAMA("Drama"),
+        TRUE_STORY("True Story"),
+        OTHERS("Others");
+
+        private String movieGenre;
+        MovieGenre(String movieGenre) {
+            this.movieGenre = movieGenre;
+        }
+    }
+
+    // AgeRestriction
+    public enum AgeRestriction {
+        G("G"), 
+        PG("PG"), 
+        PG13("PG13"), 
+        NC16("NC16"), 
+        M18("M18"), 
+        R21("R21");
+
+        private String ageRestriction;
+        AgeRestriction(String ageRestriction) {
+            this.ageRestriction = ageRestriction;
+        }
+    }
+}
---
+++ src/Classes/Movie.java
@@ -0,0 +1,73 @@
+package Classes;
+
+public class Movie {
+
+    // TODO: Add javadocs
+    
+    private MovieStatus movieStatus;
+    private String movieTitle;
+    private MovieGenre[] movieGenres;
+    private String synopsis;
+    private String director;
+    private String[] casts;
+    private int movieDuration;
+    private AgeRestriction ageRestriction;
+    private boolean is3D;
+    private double overallRating;
+    private Review[] reviews;
+
+    public Movie() {
+    }
+
+    public void setMovieStatus(MovieStatus movieStatus) {
+        this.movieStatus = movieStatus;
+    }
+
+    public void setMovieTitle(String movieTitle) {
+        this.movieTitle = movieTitle;
+    }
+
+    public void setMovieGenres(MovieGenre[] movieGenres) {
+        this.movieGenres = movieGenres;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public void setCast(String[] casts) {
+        this.casts = casts;
+    }
+
+    public void setMovieDuration(int movieDuration) {
+        this.movieDuration = movieDuration;
+    }
+
+    public void setAgeRestriction(AgeRestriction ageRestriction) {
+        this.ageRestriction = ageRestriction;
+    }
+
+    public void setIs3D(boolean is3D) {
+        this.is3D = is3D;
+    }
+
+    public void setOverallRating(double overallRating) {
+        this.overallRating = overallRating;
+    }
+
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+
+    public MovieStatus getMovieStatus() {
+        return movieStatus;
+    }
+
+    public AgeRestriction getAgeRestriction() {
+        return ageRestriction;
+    }
+    }
