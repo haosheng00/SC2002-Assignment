3944b828683757bbe5fb12433f5276aba8ad19d8
JingSiong <98008880+JingSiong@users.noreply.github.com>
2022-11-02 21:59:30 +0800

    Add files via upload

--- src/Classes/Review.java
+++ src/Classes/Review.java
@@ -1,6 +1,32 @@
-package Classes;
-
 public class Review {
     private String description;
-    private int rating;
+    private double rating;
+    private String userName;
+    private DateTime dateTime;
+    private Movie movie;
+
+    //constructor
+    public Review(String description, double rating, String userName, DateTime dateTime, Movie movie) {
+        this.description = description;
+        this.rating = rating;
+        this.userName = userName;
+        this.dateTime = dateTime;
+        this.movie = movie;
+    }
+
+    //get & set variables
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description;}
+
+    public double getRating() { return rating; }
+    public void setRating(double rating) { this.rating = rating;}
+
+    public String getUserName() { return userName; }
+    public void setUserName(String userName) { this.userName = userName;}
+
+    public DateTime getDateTime() { return dateTime; }
+    public void setDateTime(DateTime dateTime) { this.dateTime = dateTime;}
+
+    public Movie getMovie() { return movie; }
+    public void setMovie(Movie movie) { this.movie = movie;}
 }
---
+++ src/Classes/ReviewUI.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ReviewUI {
+    System.out.println("========================================");
+    System.out.println("Hi! Please select an option:");
+    System.out.println("(1) View Tickets in Cart");
+    System.out.println("(2) Checkout and Make Payment");
+        //past tickets or
+    System.out.println("(3) View Ticket History");
+    System.out.println("(4) Go Back To Main Menu");
+    System.out.println("========================================");
+    choice = sc.nextInt();
+
+
+
+
+}
