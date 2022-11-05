27b1784fcdb226c2cbaf7279982c77bf5075f5e3
killerrobbot <45598522+killerrobbot@users.noreply.github.com>
2022-11-05 14:04:04 +0800

    serialiser

---
+++ src/serialiser/SerializeMovieDB.java
@@ -0,0 +1,23 @@
+package Serialiser;
+
+import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class SerializeMovieDB {
+
+	public static List readSerializedObject(String filename) throws IOException, ClassNotFoundException{
+		FileInputStream fis = new FileInputStream(filename);
+		ObjectInputStream in = new ObjectInputStream(fis);
+		List pDetails = (ArrayList) in.readObject();
+		in.close();
+		return pDetails;
+	}
+	
+	public static void writeSerializedObject(String filename, List list) throws IOException{
+		FileOutputStream fos = new FileOutputStream(filename);
+		ObjectOutputStream out = new ObjectOutputStream(fos);
+		out.writeObject(list);
+		out.close();
+    }
+}
