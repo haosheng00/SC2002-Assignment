484eb1980da1e4794c208c8eaf969be3c62de187
killerrobbot <45598522+killerrobbot@users.noreply.github.com>
2022-11-06 21:30:48 +0800

    removed controllers

--- src/Initialiser/Initialise.java
+++ src/Initialiser/Initialise.java
@@ -13,37 +13,19 @@ public class Initialise {
     public static final int Plat_Capacity = 22;
 
     public static DateTime dt = new DateTime();
-
     public static ArrayList<Cineplex> cineplexes = new ArrayList<Cineplex>();
-    //public static ScreeningController screeningController = new ScreeningController();
-    
     public static ArrayList<Payment> payments = new ArrayList<Payment>();
-    //public static PaymentController pc = new PaymentController(payments);
-
     public static ArrayList<Review> reviews = new ArrayList<Review>();
-    //public static ReviewController rc = new ReviewController(reviews);
-    
     public static ArrayList<Customer> customers = new ArrayList<Customer>();
-    //public static CustomerController cc = new CustomerController(customers);
-    
     public static ArrayList<Admin> admins = new ArrayList<Admin>();
-    //public static AdminController ac = new AdminController(admins);
-    
-    /* Initialise MovieController */
     public static ArrayList<Movie> movies = new ArrayList<Movie>();
-    //public static MovieController mc = new MovieController();
     public static ArrayList<Movie> top5BySales = new ArrayList<Movie>();
     public static ArrayList<Movie> top5ByRatings = new ArrayList<Movie>();
-
-
-
-    
 
     //public static ArrayList<Ticket> tickets = new ArrayList<Ticket>();
     //public static ArrayList<Screening> screenings = new ArrayList<Screening>();
     //public static ArrayList<DateTime> holidays = new ArrayList<DateTime>();
     //public static TicketController tc = new TicketController(tickets, screenings, holidays);
-
 
     public static void initialiseEverything() throws Exception {
         ReadMovieDB.readMovieDB();
