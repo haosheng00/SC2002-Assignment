68a348045c4a3ed220d91ae18226bc7827d8ff75
killerrobbot <45598522+killerrobbot@users.noreply.github.com>
2022-11-06 16:39:42 +0800

    Made them return the last index + 1

--- src/Classes/DropDownMenu.java
+++ src/Classes/DropDownMenu.java
@@ -3,27 +3,33 @@ package Classes;
 import java.util.ArrayList;
 
 public class DropDownMenu {
-    public static void initiateCineplexChoice(ArrayList<Cineplex> cineplexes){
+    public static int initiateCineplexChoice(ArrayList<Cineplex> cineplexes){
+        int i;
         System.out.println("========================================");
         System.out.println("Select Cineplex: ");
-        for (int i = 0; i < cineplexes.size();i++){
+        for (i = 0; i < cineplexes.size();i++){
             System.out.println("(" + i + 1 + ") " + cineplexes.get(i).getCineplexName());
         }
+        return i;
     }
 
-    public static void initiateCinemaChoice(ArrayList<Cinema> cinemas){
+    public static int initiateCinemaChoice(ArrayList<Cinema> cinemas){
+        int i;
         System.out.println("========================================");
         System.out.println("Select Cinema: ");
-        for (int i = 0; i < cinemas.size(); i++){
+        for (i = 0; i < cinemas.size(); i++){
             System.out.println("(" + i + 1 + ")" + cinemas.get(i).getCinemaName());
         }
+        return i;
     }
 
-    public static void initiateMovieChoice(ArrayList<Movie> movies){
+    public static int initiateMovieChoice(ArrayList<Movie> movies){
+        int i;
         System.out.println("========================================");
         System.out.println("Select Movie: ");
-        for (int i = 0; i < movies.size(); i++){
-            System.out.println("(" i + 1 + ")" + movies.get(i).getMovieTitle());
+        for (i = 0; i < movies.size(); i++){
+            System.out.println("(" + i + 1 + ")" + movies.get(i).getMovieTitle());
         }
+        return i;
     }
 }
