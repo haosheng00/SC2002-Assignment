d11563052aeeb0df3fd1ee0cfb79520f7607b5ff
killerrobbot <45598522+killerrobbot@users.noreply.github.com>
2022-11-06 16:34:16 +0800

    Print heading

--- src/Classes/DropDownMenu.java
+++ src/Classes/DropDownMenu.java
@@ -4,18 +4,24 @@ import java.util.ArrayList;
 
 public class DropDownMenu {
     public static void initiateCineplexChoice(ArrayList<Cineplex> cineplexes){
+        System.out.println("========================================");
+        System.out.println("Select Cineplex: ");
         for (int i = 0; i < cineplexes.size();i++){
             System.out.println("(" + i + 1 + ") " + cineplexes.get(i).getCineplexName());
         }
     }
 
     public static void initiateCinemaChoice(ArrayList<Cinema> cinemas){
+        System.out.println("========================================");
+        System.out.println("Select Cinema: ");
         for (int i = 0; i < cinemas.size(); i++){
             System.out.println("(" + i + 1 + ")" + cinemas.get(i).getCinemaName());
         }
     }
 
     public static void initiateMovieChoice(ArrayList<Movie> movies){
+        System.out.println("========================================");
+        System.out.println("Select Movie: ");
         for (int i = 0; i < movies.size(); i++){
             System.out.println("(" i + 1 + ")" + movies.get(i).getMovieTitle());
         }
