2e86be0378922e412e0c3f0a7da9f339a15be2c8
killerrobbot <45598522+killerrobbot@users.noreply.github.com>
2022-11-06 16:52:05 +0800

    Update with everything

--- src/Classes/DropDownMenu.java
+++ src/Classes/DropDownMenu.java
@@ -1,45 +1,71 @@
 package Classes;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class DropDownMenu {
+    static Scanner sc = new Scanner(System.in);
     public static int initiateCineplexChoice(ArrayList<Cineplex> cineplexes){
-        int i;
-        System.out.println("========================================");
-        System.out.println("Select Cineplex: ");
-        for (i = 0; i < cineplexes.size();i++){
-            System.out.println("(" + i + 1 + ") " + cineplexes.get(i).getCineplexName());
-        }
-        return i;
+        int i, cineplexChoice;
+        do {
+            System.out.println("========================================");
+            System.out.println("Select Cineplex: ");
+            for (i = 0; i < cineplexes.size(); i++) {
+                System.out.println("(" + i + 1 + ")" + cineplexes.get(i).getCineplexName());
+            }
+            cineplexChoice = sc.nextInt();
+            if (cineplexChoice < 0 || cineplexChoice > i) {
+                System.out.println("Invalid Input! Try again");
+            }
+        } while (cineplexChoice < 0 || cineplexChoice > i);
+        return cineplexChoice;
     }
 
     public static int initiateCinemaChoice(ArrayList<Cinema> cinemas){
-        int i;
-        System.out.println("========================================");
-        System.out.println("Select Cinema: ");
-        for (i = 0; i < cinemas.size(); i++){
-            System.out.println("(" + i + 1 + ")" + cinemas.get(i).getCinemaName());
-        }
-        return i;
+        int i, cinemaChoice;
+        do {
+            System.out.println("========================================");
+            System.out.println("Select Cinema: ");
+            for (i = 0; i < cinemas.size(); i++) {
+                System.out.println("(" + i + 1 + ")" + cinemas.get(i).getCinemaName());
+            }
+            cinemaChoice = sc.nextInt();
+            if (cinemaChoice < 0 || cinemaChoice > i) {
+                System.out.println("Invalid Input! Try again");
+            }
+        } while (cinemaChoice < 0 || cinemaChoice > i);
+        return cinemaChoice;
     }
 
     public static int initiateScreeningChoice(ArrayList<Screening> screenings){
-        int i;
-        System.out.println("========================================");
-        System.out.println("Select Screening: ");
-        for (i = 0; i < screenings.size(); i++){
-            System.out.println("(" + i + 1 + ")" + screenings.get(i).getShowDateTime());
-        }
-        return i;
+        int i, screeningChoice;
+        do {
+            System.out.println("========================================");
+            System.out.println("Select Screening: ");
+            for (i = 0; i < screenings.size(); i++) {
+                System.out.println("(" + i + 1 + ")" + screenings.get(i).getShowDateTime());
+            }
+            screeningChoice = sc.nextInt();
+            if (screeningChoice < 0 || screeningChoice > i) {
+                System.out.println("Invalid Input! Try again");
+            }
+        } while (screeningChoice < 0 || screeningChoice > i);
+        return screeningChoice;
     }
 
-    public static int initiateMovieChoice(ArrayList<Movie> movies){
-        int i;
-        System.out.println("========================================");
-        System.out.println("Select Movie: ");
-        for (i = 0; i < movies.size(); i++){
-            System.out.println("(" + i + 1 + ")" + movies.get(i).getMovieTitle());
-        }
-        return i;
+    public static int initiateMovieChoice(ArrayList<Movie> movies) {
+        int i, movieChoice;
+        do {
+            System.out.println("========================================");
+            System.out.println("Select Movie: ");
+            for (i = 0; i < movies.size(); i++) {
+                System.out.println("(" + i + 1 + ")" + movies.get(i).getMovieTitle());
+            }
+            movieChoice = sc.nextInt();
+            if (movieChoice < 0 || movieChoice > i) {
+                System.out.println("Invalid Input! Try again");
+            }
+        } while (movieChoice < 0 || movieChoice > i);
+        return movieChoice;
     }
 }
