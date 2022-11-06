427454d4fb3e7266ae5f0d76a8948f6daa10b035
JingSiong <yujingsiong@gmail.com>
2022-11-07 03:30:16 +0800

    Add Listing Controller (UI inside)

---
+++ src/controller/ListingController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.util.*;
+
+public class ListingController {
+
+    private static int listingCriteria;
+
+    private static void setListingCriteria(int listingCriteria) {
+        ListingController.listingCriteria = listingCriteria;
+    }
+
+    public static int getListingCriteria() {
+        return listingCriteria;
+    }
+
+    public static void initiateListingUI() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("========================================");
+        System.out.println("Please select listing control");
+        System.out.println("(1) - Sales");
+        System.out.println("(2) - Rating");
+        System.out.println("(3) - Sales & Rating");
+        int listingChoice = sc.nextInt();
+
+        ListingController.setListingCriteria(listingChoice);
+    }
+}    
