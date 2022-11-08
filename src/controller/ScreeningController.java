d29905e59a8fa967873d8d610bd2282473545675
killerrobbot <45598522+killerrobbot@users.noreply.github.com>
2022-11-09 02:54:00 +0800

    back option in drop down returns -1 if back chosen

--- src/controller/ScreeningController.java
+++ src/controller/ScreeningController.java
@@ -15,15 +15,24 @@ public class ScreeningController {
     public ScreeningController(){};
     public static void addScreening(ArrayList<Cineplex> cineplexes) throws IOException{
         int cineplexChoice = DropDownMenu.initiateCineplexChoice(cineplexes);
+        if (cineplexChoice == -1){
+            return;
+        }
         Cineplex cineplexChosen = cineplexes.get(cineplexChoice);
         int cinemaChoice = DropDownMenu.initiateCinemaChoice(cineplexChosen.getCinemas());
+        if (cinemaChoice == -1){
+            return;
+        }
+        Cinema cinemaChosen = cineplexChosen.getCinemas().get(cinemaChoice);
         int movieChoice = DropDownMenu.initiateMovieChoice(cineplexChosen, 1);
+        if (movieChoice == -1){
+            return;
+        }
+        Movie movieChosen = cineplexChosen.getMovies().get(movieChoice);
         System.out.println("Add date(YYYYMMDD): ");
         String dateChosen = sc.next();
         System.out.println("Add time(HHmmss): ");
         String timeChosen = sc.next();
-        Cinema cinemaChosen = cineplexChosen.getCinemas().get(cinemaChoice);
-        Movie movieChosen = cineplexChosen.getMovies().get(movieChoice);
         Initialise.screenings.add(new Screening(cinemaChosen,dateChosen,timeChosen,movieChosen));
         SerializeMovieDB.writeSerializedObject("Screening.dat", Initialise.screenings);
         System.out.println("Screening added successfully");
@@ -31,7 +40,13 @@ public class ScreeningController {
 
     public static void deleteScreening(ArrayList<Cineplex> cineplexes) throws IOException{
         int cineplexChoice = DropDownMenu.initiateCineplexChoice(cineplexes);
+        if (cineplexChoice == -1){
+            return;
+        }
         int movieChoice = DropDownMenu.initiateMovieChoice(cineplexes.get(cineplexChoice), 1);
+        if (movieChoice == -1){
+            return;
+        }
         Movie movieChosen = cineplexes.get(cineplexChoice).getMovies().get(movieChoice);
         int screeningChoice = DropDownMenu.initiateScreeningChoice(Initialise.screenings, movieChosen);
         Initialise.screenings.remove(screeningChoice);
@@ -41,12 +56,22 @@ public class ScreeningController {
 
     public static void showScreening(ArrayList<Cineplex> cineplexes){
         int cineplexChoice = DropDownMenu.initiateCineplexChoice(cineplexes);
+        if (cineplexChoice == -1){
+            return;
+        }
         int movieChoice = DropDownMenu.initiateMovieChoice(cineplexes.get(cineplexChoice), 1);
+        if (movieChoice == -1){
+            return;
+        }
         Movie movieChosen = cineplexes.get(cineplexChoice).getMovies().get(movieChoice);
         int screeningChoice = DropDownMenu.initiateScreeningChoice(Initialise.screenings, movieChosen);
-        System.out.println("Printing screening information for Movie " + movieChosen.getMovieTitle());
+        if (screeningChoice == -1){
+            return;
+        }
+        System.out.println("Printing screening information for Movie... " + movieChosen.getMovieTitle());
         System.out.println("Cinema: " + Initialise.screenings.get(screeningChoice).getCinema().getCinemaName());
-        System.out.println("ShowTime: " +Initialise.screenings.get(screeningChoice).getShowDate() + " " + Initialise.screenings.get(screeningChoice-1).getShowTime());
+        System.out.println("Show Date: " +Initialise.screenings.get(screeningChoice).getShowDate());
+        System.out.println("Show Time: " + Initialise.screenings.get(screeningChoice).getShowTime());
         System.out.println();
     }
 }
