f22392a17d5dc300ae0766d8d287055fa3534d9f
Jessica1510 <103560613+Jessica1510@users.noreply.github.com>
2022-11-08 14:34:36 +0800

    modified:   src/Initialiser/Initialise.java

--- src/Initialiser/Initialise.java
+++ src/Initialiser/Initialise.java
@@ -18,15 +18,8 @@ public class Initialise {
     public static ArrayList<Cineplex> cineplexes = new ArrayList<Cineplex>();
     //cineplexes.add(new Cineplex(null, null,null));
     public static ArrayList<Payment> payments = new ArrayList<Payment>();
-<<<<<<< HEAD
     public static ArrayList<Review> reviews = new ArrayList<Review>();
     public static ArrayList<Customer> customers = new ArrayList<Customer>(List.of(new Customer("guest", "guest", "123", "123"), new Customer("mary", "mary123", "hh", "123")));
-=======
-    public static ArrayList<Review> reviews = new ArrayList<Review>(); 
-    //reviews.add(new Review())
-    public static ArrayList<Customer> customers = new ArrayList<Customer>();
-    //(List.of(new Customer("hello", "1234", "yay@gmail.com", "9091920")));
->>>>>>> 38ea56fa29db73d1e3433546cf319e2c112a516f
     public static ArrayList<Admin> admins = new ArrayList<Admin>(List.of(new Admin("admin1", "admin1"), new Admin("admin2", "admin2")));
     public static ArrayList<Movie> movies = new ArrayList<Movie>(List.of(new Movie("Joy"), new Movie("Sad"), new Movie("Fear")));
     public static ArrayList<Screening> screenings = new ArrayList<Screening>();
