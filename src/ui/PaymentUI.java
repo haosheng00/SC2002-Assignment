a912be11d81f010a1c0b07f14150e3311422f3c0
JingSiong <yujingsiong@gmail.com>
2022-11-12 00:48:07 +0800

    edits

--- src/ui/PaymentUI.java
+++ src/ui/PaymentUI.java
@@ -11,28 +11,36 @@ public class PaymentUI {
         Scanner sc = new Scanner(System.in);
         int choice = 0;
 
-        while (choice != 4) {
-            System.out.println("========================================");
-            System.out.println("Hi! Please select an option:");
-            System.out.println("(1) View Tickets in Cart");
-            System.out.println("(2) Checkout and Make Payment");
-            System.out.println("(3) Go Back To Main Menu");
-            System.out.println("========================================");
+        System.out.println("========================================");
+        System.out.println("Please select an option:");
+        System.out.println("(1) View Tickets in Cart");
+        System.out.println("(2) Checkout and Make Payment");
+        System.out.println("(3) Go Back To Main Menu");
+        System.out.println("========================================");
+        
+        do {
             choice = sc.nextInt();
-            switch(choice) {
-                case 1:
-                    PaymentController.showTickets(customer.getCartTickets());
-                    break;
-                case 2:
-                    PaymentController.checkoutUI(customer);
-                    break;
-                case 3:
-                    CustomerMenuUI.customerMenuOptions(customer);
-                    //return;
-                default:
-                    System.out.println("Invalid input!");
-                    break;
+            try {
+                switch(choice) {
+                    case 1:
+                        PaymentController.showTickets(customer.getCartTickets());
+                        break;
+                    case 2:
+                        PaymentController.checkoutUI(customer);
+                        break;
+                    case 3:
+                        CustomerMenuUI.customerMenuOptions(customer);
+                        //return;
+                    default:
+                        System.out.println("Invalid option. Please enter an integer (1-3): ");
+                        break;
+                    }    
             }
-        }
+            catch (Exception e) {
+                System.out.println("Invalid input. Please enter an integer: ");
+                sc.next();
+            }
+        } while (true);
     }
+        
 }
