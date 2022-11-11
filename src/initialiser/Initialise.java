b58e19e99f1949160e9e8769bd228bd26b279275
haosheng00 <kohhaosheng@gmail.com>
2022-11-11 17:34:02 +0800

    initialise

--- src/initialiser/Initialise.java
+++ src/initialiser/Initialise.java
@@ -16,14 +16,7 @@ public class Initialise {
 
     public static DateTime dt = new DateTime();
 
-<<<<<<< HEAD
-    public static double [] priceByAge = new double [] {}; 
-    public static double [] priceByMovieType = new double [] {};
-    public static double [] priceByCinemaType = new double [] {};
-    public static double [] priceByDay = new double [] {}; 
-=======
     public static int listingCriteria;
->>>>>>> 56a1d48a5ce690ef8a423b1df1bbb291032c1923
 
     public static ArrayList<Cineplex> cineplexes = new ArrayList<Cineplex>();
     public static ArrayList<Payment> payments = new ArrayList<Payment>();
@@ -35,6 +28,11 @@ public class Initialise {
     public static ArrayList<Movie> top5BySales = new ArrayList<Movie>();
     public static ArrayList<Movie> top5ByRatings = new ArrayList<Movie>();
     public static ArrayList<PublicHoliday> holidays = new ArrayList<PublicHoliday>();
+
+    public static double [] priceByAge = new double [] {}; //student, adult, senior
+	public static double [] priceByMovieType = new double [] {}; //2d, 3d
+	public static double [] priceByCinemaType = new double [] {}; //ordinary, premium
+	public static double [] priceByDay = new double [] {}; //weekday, weekend, holiday
 
 
     public static void initialiseMOBLIMA() throws Exception {
